75b3579d430f496906b51d370216f43331db1f93
hardreggaecafe <takamizawa@frembassy.jp>
2023-03-11 18:17:28 +0900

    Erase github integration

--- server/api/auth/[...].ts
+++ server/api/auth/[...].ts
@@ -3,11 +3,6 @@ import GithubProvider from 'next-auth/providers/github'
 export default NuxtAuthHandler({
     secret: 'my-superb-secret',
     providers: [
-        // @ts-expect-error You need to use .default here for it to work during SSR. May be fixed via Vite at some point
-        GithubProvider.default({
-           clientId: '8ef1f641b5705b75373c',
-           clientSecret: 'db83a5af49555525899468b7e5247e6dcc11aed2'
-        })
     ]
 })
 
