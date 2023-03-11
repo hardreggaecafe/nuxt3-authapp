0470d3272887e61cb352af0a2a5922a9b72548d2
hardreggaecafe <takamizawa@frembassy.jp>
2023-03-11 18:13:58 +0900

    Fix bug

--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -3,7 +3,7 @@ import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 import { MAIN_THEME, mainTheme, MAIN_DARK_THEME, mainDarkTheme } from '@/helpers/themes'
 import { defaults } from '@/helpers/defaults'
-import '@mdi/font/css/materialdesignicons.css' 
+//import '@mdi/font/css/materialdesignicons.css' 
 
 export default defineNuxtPlugin(nuxtApp => {
   const vuetify = createVuetify({
