e0afab06d3a6a72ffbda427ee0327b961f59db0a
hardreggaecafe <takamizawa@frembassy.jp>
2023-04-13 00:53:54 +0900

    WIP

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -2,7 +2,7 @@ export default defineNuxtRouteMiddleware(async () => {
   if (!process.server) {
     const { checkAuthState, token } = useAuth()
     await checkAuthState()
-    if (!token.value) {
+    if (!token.value && currentUser.emailVerified) {
       return navigateTo('/login', { replace: true })
     }
   }
